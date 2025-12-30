753f31be40552117b0f97e5d8b1e7f77a1636295
dmihovch <dmihovch@gmail.com>
2025-12-30 14:49:34 -0500

    multiple windows via panels

--- portfolio/main.c
+++ portfolio/main.c
@@ -1,7 +1,6 @@
 #include "include/animations.h"
-#include "include/utils.h"
 #include <string.h>
-
+#include <panel.h>
 
 #define NUMBOLTS 5
 
@@ -11,36 +10,68 @@ int main(int argc, char* argv[]){
 	int minx = 1;
 	int maxy = getmaxy(stdscr)-2;
 	int maxx = getmaxx(stdscr)-2;
-	Lightning bolts[NUMBOLTS];
-	for(int i = 0; i<NUMBOLTS; i++){
-		bolts[i] = createLightning(10+i*20);
-	}
+	//
+	//
+	// Lightning bolts[NUMBOLTS];
+	// for(int i = 0; i<NUMBOLTS; i++){
+	// 	bolts[i] = createLightning(10+i*20);
+	// }
+	//
+	// const char* msg = "Daniel Mihovch";
+	// int len = strlen(msg);
+	// CycleLetter* str = createCycleString(msg, len, 10, 10, 10);
+	// if(str == NULL) return 1;
+	//
+
+	WINDOW* win = newwin(0,0,0,0);
+	WINDOW* win2 = newwin(0,0,0,0);
+	PANEL* pan = new_panel(win);
+	PANEL* pan2 = new_panel(win2);
+
+
 	
-
-
-
-		const char* msg = "Daniel Mihovch";
-		int len = strlen(msg);
-		CycleLetter* str = createCycleString(msg, len, 10, 10, 100);
-		if(str == NULL) return 1;
-
-	while(1 ){
+	box(win, 0, 0);
+	mvwprintw(win, 10,10, "WIN1 GOOBYE WORLD");
+	mvwprintw(win2, 20,20, "WIN2 HELLO WORLD");
+		
+	while(3){
 		char ch = getch();
 		if(ch == 'q') break;
-
-
-		for(int i = 0; i<NUMBOLTS; i++){
-		 	if(bolts[i].y < maxy) updateLightning(&bolts[i], maxy,maxx );
+		if(ch == '1'){
+			top_panel(pan);
+		} 
+		if(ch == '2'){
+			top_panel(pan2);
 		}
+		update_panels();
+		doupdate();
 		
-		updateCycleString(str, len);
+		
 
 
-		refresh();
+
+
+		
+
+
+
+
+
+
+
+
+
+		//
+		// for(int i = 0; i<NUMBOLTS; i++){
+		// 	if(bolts[i].y < maxy) updateLightning(&bolts[i], maxy,maxx );
+		// }
+		//
+		// updateCycleString(str, len);
+		
+	
 		usleep(50000);
-	}
 
-	free(str);
+	}
 
 	closeCurses();
 	return 0;
